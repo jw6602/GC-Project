57e236f237a090a604de90e2840274034705f8b8
jw6602 <jw6602@nyu.edu>
2021-10-28 13:44:10 -0400

    update PMFG

--- PMFG.py
+++ PMFG.py
@@ -1,6 +1,8 @@
 import networkx
 from typing import List
 import planarity
+import itertools
+import threading
 
 class edge():
     """
@@ -37,14 +39,46 @@ class PMFG():
             self.sort_edge()
         number_of_nodes = self.origin_graph.number_of_nodes()
         pmfg_graph = networkx.Graph()
-        for edge in self.sort_edges:
-            # Adding edge and check the planarity
-            pmfg_graph.add_edge(edge.src, edge.dst, weight=edge.wt)
-            # If the graph is not planar, then remove the edge
-            if not self.is_planar(pmfg_graph, self.planarity_check_lib):
-                pmfg_graph.remove_edge(edge.src, edge.dst)
+        i = 0
+        while i < len(self.sort_edges):
+            if pmfg_graph.number_of_edges() < 3 * (number_of_nodes - 2): # we will only use single thread for now
+                edge = self.sort_edges[i]
+                pmfg_graph.add_edge(edge.src, edge.dst, weight=edge.wt)
+                if not self.is_planar(pmfg_graph, self.planarity_check_lib):
+                    pmfg_graph.remove_edge(edge.src, edge.dst)
+                i += 1
+            else:
+                # when the size of the graph is small, it is better to use single thread b/c planarity check is too fast
+                # when the size of the graph is large enough, the multi-threading should have better performance
+                step_length = 95
+                edges = self.sort_edges[i:i+step_length]
+                results = [False] * len(edges)
+                threads = []
+                def forward_one_step_is_planar(graph, index, edge):
+                    graph.add_edge(edge.src, edge.dst, weight=edge.wt)
+                    results[index] = planarity.is_planar(graph)
+                for index, edge in enumerate(edges):
+                    threads.append(threading.Thread(target=forward_one_step_is_planar, args=(pmfg_graph.copy(), index, edge)))
+                for _t in threads:
+                    _t.start()
+                for _t in threads:
+                    _t.join()
+                available_edges = []
+                for index, ok in enumerate(results):
+                    if ok:
+                        available_edges.append(edges[index])
+                edges_combinations = self.combinations(available_edges)
+                for edges_combination in edges_combinations:
+                    for edge in edges_combination:
+                        pmfg_graph.add_edge(edge.src, edge.dst, weight=edge.wt)
+                    if self.is_planar(pmfg_graph, self.planarity_check_lib):
+                        break
+                    else:
+                        for edge in edges_combination:
+                            pmfg_graph.remove_edge(edge.src, edge.dst)
+                i += step_length
             if self.verbose:
-                    print(f"Number of edges added = {pmfg_graph.number_of_edges()}, Number of edges to be added = {3 * (number_of_nodes - 2) - pmfg_graph.number_of_edges()}")
+                print(f"Number of edges added = {pmfg_graph.number_of_edges()}, Number of edges to be added = {3 * (number_of_nodes - 2) - pmfg_graph.number_of_edges()}")
             if pmfg_graph.number_of_edges() == 3 * (number_of_nodes - 2):
                 break
         self.pmfg_graph = pmfg_graph
@@ -55,3 +89,10 @@ class PMFG():
         if planarity_check_lib == "networkx":
             return networkx.algorithms.planarity.check_planarity(graph)[0]
         return planarity.is_planar(graph)
+    
+    def combinations(iterable):
+        r = []
+        for i in reversed(range(1, len(iterable)+1)):
+            for subset in itertools.combinations(iterable, i):
+                r.append(subset)
+        return r
